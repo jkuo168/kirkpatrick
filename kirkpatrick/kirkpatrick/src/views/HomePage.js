1881ae3d1edb7922d5a081b4808cb0ea7b55edb3
Joanna Kuo <jkuo@princeton.edu>
2021-12-11 13:17:56 -0500

    hosting

---
+++ kirkpatrick/kirkpatrick/src/views/HomePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+
+export default function HomePage() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "background.default",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ textAlign: "center" }}>
+        <Typography variant="h2" sx={{ color: "white" }}>
+          Kirkpatrick's Algorithm
+        </Typography>
+        <Button variant="contained" href="/demo" sx={{ mt: 10 }}>
+          Demo
+        </Button>
+      </Box>
+    </Box>
+  );
+}
